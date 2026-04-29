6202623b24e71f4375d00001d71ece3ce2d8974c
Jithin K Sunil <jithinksunil96@gmail.com>
2026-04-29 20:48:49 +0530

    feat: refactor exception handling by moving AllExceptionsFilter to utility folder

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { AllExceptionsFilter } from './utility/all-exceptions.filter';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
--- src/utility/all-exceptions.filter.ts
+++ src/utility/all-exceptions.filter.ts
@@ -6,7 +6,7 @@ import {
   HttpStatus,
   Logger,
 } from '@nestjs/common';
-import { Prisma } from '../../generated/prisma/client';
+import { Prisma } from '../generated/prisma/client';
 import { Request, Response } from 'express';
 
 interface ErrorResponse {
